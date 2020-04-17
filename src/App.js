7ab1f25cf12fe1eb33e65c40f79f2788c3b199fc
Vivek S <vivekkalsa009@gmail.com>
2020-04-17 09:08:32 +0530

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+// showing vs-code github setup
 
 function App() {
   return (
